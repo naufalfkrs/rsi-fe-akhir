4aba06dd7e6735fd3dfdfa373cb6081254d937ea
Naufal Fakhri Risnawa <naufalfakhri.cubiconia@gmail.com>
2025-11-18 11:01:15 +0700

    add checkout

--- pages/dashboard/checkout/[id].vue
+++ pages/dashboard/checkout/[id].vue
@@ -64,83 +64,134 @@ const buyNow = () => navigateTo(`/dashboard/checkout/${productId}`);
   <div>
     <Header />
 
-    <main class="max-w-[90%] mx-auto py-10 flex gap-10">
-
+    <main class="max-w-[90%] mx-auto py-10 ">
+      <h1 class="text-4xl font-semibold mb-6 text-gray-800">Check - Out</h1>
       <!-- LEFT: IMAGE -->
-      <div class="w-2/6">
-        <img
-          :src="productDetail?.imageUrl"
-          class=" rounded-lg shadow-md object-cover w-full"
-          alt="product"
-        />
-      </div>
+      <div class="flex gap-10">
+        <!-- <div class="w-2/6">
+          <img
+            :src="productDetail?.imageUrl"
+            class=" rounded-lg shadow-md object-cover w-full"
+            alt="product"
+          />
+        </div> -->
 
-      <!-- MIDDLE: DETAILS -->
-      <div class="w-4/6 pr-10">
-        <h1 class="text-3xl font-bold text-gray-800 mb-10">
-          {{ productDetail?.name }}
-        </h1>
+        <!-- MIDDLE: DETAILS -->
+        <div class="w-5/6 bg-white shadow-md rounded-lg p-5">
+          <p class="font-base text-lg text-gray-700 mb-3">Alamat Pengiriman</p>
+          <!-- 2 COL — USER INFO -->
+          <div class="flex gap-5 mb-20">
+            <!-- LEFT -->
+            <div class="w-1/3">
+              <p class="font-bold text-gray-700 text-sm">Naufal Fakhri</p>
+              <p class="text-gray-600 text-sm mt-1">+62 812 3456 7890</p>
+            </div>
 
-        <p class="text-2xl font-semibold text-green-600 mb-5">
-          Rp {{ productDetail?.price.toLocaleString() }}
-        </p>
-
-        <h1 class="font-semibold">Deskripsi Produk</h1>
-        <p class="text-gray-700 text-sm leading-relaxed whitespace-pre-line">
-          {{ productDetail?.description }}
-        </p>
-      </div>
-
-      <!-- RIGHT: PURCHASE BOX -->
-      <div class="w-2/6 bg-white shadow-md rounded-lg p-5 h-fit">
-
-        <p class="font-semibold text-gray-700 mb-3">Atur Jumlah Pembelian</p>
-
-        <div class="flex items-center justify-between mb-6">
-          <!-- Kiri: Quantity -->
-          <div class="flex items-center gap-3 w-2/5">
-
-            <button
-              class="w-8 h-8 bg-gray-200 rounded flex items-center justify-center text-lg font-bold"
-              @click="decreaseQty"
-            >
-              -
-            </button>
-
-            <span class="text-lg font-semibold w-6 text-center">
-              {{ quantity }}
-            </span>
-
-            <button
-              class="w-8 h-8 bg-gray-200 rounded flex items-center justify-center text-lg font-bold"
-              @click="increaseQty"
-            >
-              +
-            </button>
-
+            <!-- RIGHT -->
+            <div class="w-2/3">
+              <p class="text-gray-700 text-sm leading-relaxed">
+                Jl. Veteran Malang No. 15, Lowokwaru, Kota Malang, Jawa Timur 65145
+              </p>
+            </div>
           </div>
 
-          <!-- Kanan: Stok -->
-          <p class="text-sm text-gray-500 w-3/5 text-end">
-            Stok tersedia: <span class="font-semibold">{{ productDetail?.stock }}</span>
-          </p>
+          <!-- PRODUCT ORDERED -->
+
+          <!-- 3 COL GRID -->
+          <div class="grid grid-cols-3 gap-5 items-start">
+
+            <!-- LEFT (IMAGE + NAME + QTY) -->
+            <div class="w-full">
+              <p class="font-semibold text-gray-800 mb-4 text-sm">Produk Dipesan</p>
+              <div class="flex gap-3 items-center">
+                <!-- IMAGE -->
+                <img
+                  :src="productDetail?.imageUrl"
+                  class="w-20 h-20 object-cover rounded-md shadow-md"
+                  alt="product"
+                />
+
+                <!-- NAME + QTY -->
+                <div>
+                  <p class="font-semibold text-gray-800 text-sm">
+                    {{ productDetail?.name }}
+                  </p>
+
+                  <p class="text-gray-600 text-sm mt-1">
+                    Quantity: <span class="font-medium">{{ quantity }}</span>
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <!-- MIDDLE — UNIT PRICE -->
+            <div class="w-full text-end h-full">
+              <p class="font-semibold text-gray-700 text-sm">Harga Satuan</p>
+              <!-- BAGIAN YANG DI-TENGAHKAN -->
+              <div class="h-full flex items-center justify-end">
+                <p class="text-green-600 font-semibold text-sm">
+                  Rp {{ productDetail?.price.toLocaleString() }}
+                </p>
+              </div>
+            </div>
+
+            <!-- RIGHT — SUBTOTAL -->
+            <div class="w-full text-end h-full">
+              <p class="font-semibold text-gray-700 text-sm">Subtotal</p>
+              <div class="h-full flex items-center justify-end">
+                <p class="mt-2 text-green-600 font-bold text-sm">
+                  Rp {{
+                    (productDetail?.price * quantity).toLocaleString()
+                  }}
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
 
-        <!-- BUTTON: Keranjang -->
-        <button
-          class="w-full py-3 rounded-md border bg-white text-customGreen font-semibold hover:bg-gray-100 mb-3"
-          @click="goCart"
-        >
-          Tambah ke Keranjang
-        </button>
+        <!-- RIGHT: PURCHASE BOX -->
+        <div class="w-2/6 bg-white shadow-md rounded-lg p-5 h-fit">
 
-        <!-- BUTTON: Beli Sekarang -->
-        <button
-          class="w-full py-3 rounded-md bg-green-600 text-white font-semibold hover:bg-green-700"
-          @click="buyNow"
-        >
-          Beli Sekarang
-        </button>
+          <p class="font-bold text-gray-700 mb-3 text-xl">Cek Ringkasan Transaksimu</p>
+
+          <!-- ROW: Total Harga -->
+          <div class="flex justify-between text-sm">
+            <p class="text-gray-600">Total Harga</p>
+            <p class="font-medium">
+              Rp {{ (productDetail?.price * quantity).toLocaleString() }}
+            </p>
+          </div>
+
+          <!-- ROW: Biaya Pengiriman -->
+          <div class="flex justify-between text-sm">
+            <p class="text-gray-600">Biaya Pengiriman</p>
+            <p class="font-medium">Rp 10.000</p>
+          </div>
+
+          <!-- ROW: Biaya Layanan -->
+          <div class="flex justify-between text-sm">
+            <p class="text-gray-600">Biaya Layanan</p>
+            <p class="font-medium">Rp 3.000</p>
+          </div>
+
+          <!-- TOTAL TAGIHAN -->
+          <div class="flex justify-between pt-4 mb-8">
+            <p class="text-gray-800 font-semibold text-base">Total Tagihan</p>
+            <p class="text-gray-800 font-bold text-xl">
+              Rp {{
+                ((productDetail?.price * quantity) + 10000 + 3000).toLocaleString()
+              }}
+            </p>
+          </div>
+
+          <!-- BUTTON: Beli Sekarang -->
+          <button
+            class="w-full py-3 rounded-md bg-green-600 text-white font-semibold hover:bg-green-700"
+            @click="buyNow"
+          >
+            Beli Sekarang
+          </button>
+        </div>
       </div>
     </main>
   </div>
